367e94e785ad695ee67f9261431d83d299deeb84
BuildTools <unconfigured@null.spigotmc.org>
2026-04-17 18:53:51 +0200

    Sensor integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,91 @@
 #include <Arduino.h>
-#include <Adafruit_MPL3115A2.h>
-#define LED_BUILTIN 2
+#include "Clock.h"
+#include "SCD30.h"
+#include "Adafruit_MPL3115A2.h"
+#include <Adafruit_MAX31865.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define RREF      4300.0
+#define RNOMINAL  1000.0
+#define MAX3185_CS 4
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+Adafruit_MPL3115A2 baro;
+Adafruit_MAX31865 thermo = Adafruit_MAX31865(MAX3185_CS);
+void initSensors() {
+  Clock::init();
+  scd30.initialize();
+  baro.begin();
+  baro.setSeaPressure(1013.26);
+  thermo.begin(MAX31865_2WIRE);  // set to 2WIRE or 4WIRE as necessary
+}
+void testSensors() {
+  Serial.println("-----------------");
+  Clock::test();
+  float result[3] = {0};
+  if (scd30.isAvailable()) {
+        scd30.getCarbonDioxideConcentration(result);
+        Serial.println("-----------------");
+        Serial.print("Carbon Dioxide Concentration is: ");
+        Serial.print(result[0]);
+        Serial.println(" ppm");
+        Serial.print("Temperature = ");
+        Serial.print(result[1]);
+        Serial.println(" ℃");
+        Serial.print("Humidity = ");
+        Serial.print(result[2]);
+        Serial.println(" %");
+    }
+  Serial.print("\n");
+  float pressure = baro.getPressure();
+  float altitude = baro.getAltitude();
+  float temperature = baro.getTemperature();
+
+  Serial.println("-----------------");
+  Serial.print("pressure = "); Serial.print(pressure); Serial.println(" hPa");
+  Serial.print("altitude = "); Serial.print(altitude); Serial.println(" m");
+  Serial.print("temperature = "); Serial.print(temperature); Serial.println(" C");
+
+  uint16_t rtd = thermo.readRTD();
+
+  Serial.println("-----------------");
+  Serial.print("RTD value: "); Serial.println(rtd);
+  float ratio = rtd;
+  ratio /= 32768;
+  Serial.print("Ratio = "); Serial.println(ratio,8);
+  Serial.print("Resistance = "); Serial.println(RREF*ratio,8);
+  Serial.print("Temperature = "); Serial.println(thermo.temperature(RNOMINAL, RREF));
+
+    uint8_t fault = thermo.readFault();
+  if (fault) {
+    Serial.print("Fault 0x"); Serial.println(fault, HEX);
+    if (fault & MAX31865_FAULT_HIGHTHRESH) {
+      Serial.println("RTD High Threshold"); 
+    }
+    if (fault & MAX31865_FAULT_LOWTHRESH) {
+      Serial.println("RTD Low Threshold"); 
+    }
+    if (fault & MAX31865_FAULT_REFINLOW) {
+      Serial.println("REFIN- > 0.85 x Bias"); 
+    }
+    if (fault & MAX31865_FAULT_REFINHIGH) {
+      Serial.println("REFIN- < 0.85 x Bias - FORCE- open"); 
+    }
+    if (fault & MAX31865_FAULT_RTDINLOW) {
+      Serial.println("RTDIN- < 0.85 x Bias - FORCE- open"); 
+    }
+    if (fault & MAX31865_FAULT_OVUV) {
+      Serial.println("Under/Over voltage"); 
+    }
+    thermo.clearFault();
+  }
 }
 
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  initSensors();
 }
-
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // change state of the LED by setting the pin to the HIGH voltage level
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // change state of the LED by setting the pin to the LOW voltage level
-  delay(1000);                      // wait for a second
-  int result = myFunction(2, 3);
+  testSensors();
+  Serial.println("IMG180230");
+
+  delay(5000);
 }
